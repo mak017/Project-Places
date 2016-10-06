b1617b0ba4a0040eb5b78534cedbe0a66d5f99cb
Denis Makovskyy <makovskyy0983@gmail.com>
2016-10-07 00:59:24 +0300

    mie place added, +some minor improves to main page

--- public/js/main.js
+++ public/js/main.js
@@ -2,3 +2,18 @@ $("h2").slabText({
             // Don't slabtext the headers if the viewport is under 380px
             "viewportBreakpoint":380
 });
+
+$('#myTab a').click(function (e) {
+  e.preventDefault()
+  $(this).tab('show')
+})
+$('a[data-toggle="tab"]').on('shown.bs.tab', function (e) {
+  e.target // activated tab
+  e.relatedTarget // previous tab
+})
+$('#myTab a[href="#main-info"]').tab('show')
+$('#myTab a[href="#when"]').tab('show')
+$('#myTab a[href="#how"]').tab('show')
+$('#myTab a[href="#hotels"]').tab('show')
+$('#myTab a[href="#what-to-do"]').tab('show')
+$('#myTab a[href="#photo"]').tab('show')
