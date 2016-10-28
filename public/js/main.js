2a9644d9e4919d7576a50e9744461360643dc54a
Denis Makovskyy <makovskyy0983@gmail.com>
2016-10-28 17:59:42 +0300

    major improvements to main page and Mie place page

--- public/js/main.js
+++ public/js/main.js
@@ -1,19 +1,15 @@
-$("h2").slabText({
-            // Don't slabtext the headers if the viewport is under 380px
-            "viewportBreakpoint":380
-});
-
-$('#myTab a').click(function (e) {
-  e.preventDefault()
-  $(this).tab('show')
-})
-$('a[data-toggle="tab"]').on('shown.bs.tab', function (e) {
-  e.target // activated tab
-  e.relatedTarget // previous tab
-})
+// Tabs on place page
 $('#myTab a[href="#main-info"]').tab('show')
 $('#myTab a[href="#when"]').tab('show')
 $('#myTab a[href="#how"]').tab('show')
 $('#myTab a[href="#hotels"]').tab('show')
 $('#myTab a[href="#what-to-do"]').tab('show')
 $('#myTab a[href="#photo"]').tab('show')
+
+// Search
+var places = []
+places.filter(findPlace)
+function findPlace(str, item) {
+    item.place.isInclude(str)
+}
+var results = places.filter()
