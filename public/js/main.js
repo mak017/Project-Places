034a566f9e587d683279d66e7aae52f64cfd9ed2
mak017 <makovskyy0983@gmail.com>
2016-10-30 17:05:57 +0200

    minor improvements

--- public/js/main.js
+++ public/js/main.js
@@ -1,11 +1,3 @@
-// Tabs on place page
-$('#myTab a[href="#main-info"]').tab('show')
-$('#myTab a[href="#when"]').tab('show')
-$('#myTab a[href="#how"]').tab('show')
-$('#myTab a[href="#hotels"]').tab('show')
-$('#myTab a[href="#what-to-do"]').tab('show')
-$('#myTab a[href="#photo"]').tab('show')
-
 // Search
 var places = []
 places.filter(findPlace)
