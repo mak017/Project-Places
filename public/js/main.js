444d0519c95110cb789a2d69c836d1df0f18bfda
Denis Makovskyy <makovskyy0983@gmail.com>
2016-11-04 17:13:34 +0200

    header buttons are animated now, header layout changed

--- public/js/main.js
+++ public/js/main.js
@@ -1,12 +1,35 @@
-// Tabs on place page
-$('#myTab a[href="#main-info"]').tab('show')
-$('#myTab a[href="#when"]').tab('show')
-$('#myTab a[href="#how"]').tab('show')
-$('#myTab a[href="#hotels"]').tab('show')
-$('#myTab a[href="#what-to-do"]').tab('show')
-$('#myTab a[href="#photo"]').tab('show')
+// Search (under construction)
+places = [{
+"city": "Mie",
+"country": "Japan",
+"url": "../pages/places/mie.html"
+},
+{}
+]
+$(document).ready(function(){
+    $(function () {
+    $('.header__search-box').typeahead({
+        name: 'Location',
+        local: [{
+            value: 'Bangkok',
+            tokens: ['Bang', 'Cock'],
+            id: 2
+        }, {
+            value: 'Kuala Lumpur',
+            tokens: ['Kuala', 'Lumpur', 'la'],
+            id: 3
+        }]
+    });
 
-// Search
+    $('.header__search-box').on('header__search-box:selected', function (e, datum) {
+        $('.fake-hidden').val(datum.id);
+    
+}); 
+
+
+
+
+
 var places = []
 places.filter(findPlace)
 function findPlace(str, item) {
---
+++ public/js/places.js
@@ -0,0 +1,7 @@
+// Tabs on place page
+$('#myTab a[href="#main-info"]').tab('show')
+$('#myTab a[href="#when"]').tab('show')
+$('#myTab a[href="#how"]').tab('show')
+$('#myTab a[href="#hotels"]').tab('show')
+$('#myTab a[href="#what-to-do"]').tab('show')
+$('#myTab a[href="#photo"]').tab('show')
