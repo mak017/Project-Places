5072635b085432701e9b7c8743e5796b23988d87
Denis Makovskyy <makovskyy0983@gmail.com>
2016-11-10 19:57:18 +0200

    typeahead search now works perfectly

--- public/js/main.js
+++ public/js/main.js
@@ -1,62 +1,55 @@
-// Search (under construction)
+// Search
 places = [{
-"city": "Mie",
-"country": "Japan",
-"url": "../pages/places/mie.html"
-},
-{}
+    "city": "Mie",
+    "country": "Japan",
+    "img": "images/content/places/mie/mie-icon.png",
+    "url": "pages/places/mie.html"
+    },
+    {}
 ]
-var substringMatcher = function(strs) {
-  return function findMatches(q, cb) {
-    var matches, substringRegex;
-
-    // an array that will be populated with substring matches
-    matches = [];
-
-    // regex used to determine if a string contains the substring `q`
-    substrRegex = new RegExp(q, 'i');
-
-    // iterate through the pool of strings and for any string that
-    // contains the substring `q`, add it to the `matches` array
-    $.each(strs, function(i, str) {
-      if (substrRegex.test(str)) {
-        matches.push(str);
-      }
-    });
-
-    cb(matches);
-  };
-};
-
-var states = ['Alabama', 'Alaska', 'Arizona', 'Arkansas', 'California',
-  'Colorado', 'Connecticut', 'Delaware', 'Florida', 'Georgia', 'Hawaii',
-  'Idaho', 'Illinois', 'Indiana', 'Iowa', 'Kansas', 'Kentucky', 'Louisiana',
-  'Maine', 'Maryland', 'Massachusetts', 'Michigan', 'Minnesota',
-  'Mississippi', 'Missouri', 'Montana', 'Nebraska', 'Nevada', 'New Hampshire',
-  'New Jersey', 'New Mexico', 'New York', 'North Carolina', 'North Dakota',
-  'Ohio', 'Oklahoma', 'Oregon', 'Pennsylvania', 'Rhode Island',
-  'South Carolina', 'South Dakota', 'Tennessee', 'Texas', 'Utah', 'Vermont',
-  'Virginia', 'Washington', 'West Virginia', 'Wisconsin', 'Wyoming'
-];
-
-$('.typeahead').typeahead({
-  hint: true,
-  highlight: true,
-  minLength: 1
-},
-{
-  name: 'states',
-  source: substringMatcher(states)
-});
 
 
+$(document).ready(function() {
 
+        // Set the Options for "Bloodhound" Engine
+                var my_Suggestion_class = new Bloodhound({
+                    limit: 10,
+                    datumTokenizer: Bloodhound.tokenizers.obj.whitespace('value'),
+                    queryTokenizer: Bloodhound.tokenizers.whitespace,
+                    local: $.map(places, function(item) {
+                        //console.log(item.city);
+                        return {
+                            value: item.city,
+                            country: item.country,
+                            img: item.img,
+                            url: item.url
+                        };
+                    })
+                });
 
+        //Initialize the Suggestion Engine
+                my_Suggestion_class.initialize();
 
-
-/* var places = []
-places.filter(findPlace)
-function findPlace(str, item) {
-    item.place.isInclude(str)
-}
-var results = places.filter() */
+                var typeahead_elem = $('.typeahead');
+                typeahead_elem.typeahead({
+                    hint: true,
+                    highlight: true,
+                    minLength: 1
+                },
+                {
+                    name: 'value',
+                    displayKey: 'value',
+                    source: my_Suggestion_class.ttAdapter(),
+                    templates: {
+                        empty: [
+                            '<div class="noitems">',
+                            'No Items Found',
+                            '</div>'
+                        ].join('\n'),
+                        suggestion: function (data) {
+                            return '<a href="' + data.url + '"><img src="' + data.img + '"><span>' + data.value + ', ' + data.country + '</span></a>';
+                        } 
+                           
+                    }
+                });
+            });
