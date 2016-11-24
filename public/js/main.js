788fedede5424a5dfca3d3d807183cbd543d8553
Denis Makovskyy <makovskyy0983@gmail.com>
2016-11-25 00:41:14 +0200

    added header mobile responsiveness

--- public/js/main.js
+++ public/js/main.js
@@ -1,4 +1,5 @@
-// Search
+// Typeahead search
+
 places = [{
     "city": "Mie",
     "country": "Japan",
@@ -8,48 +9,70 @@ places = [{
     {}
 ]
 
-
 $(document).ready(function() {
 
-        // Set the Options for "Bloodhound" Engine
-                var my_Suggestion_class = new Bloodhound({
-                    limit: 10,
-                    datumTokenizer: Bloodhound.tokenizers.obj.whitespace('value'),
-                    queryTokenizer: Bloodhound.tokenizers.whitespace,
-                    local: $.map(places, function(item) {
-                        //console.log(item.city);
-                        return {
-                            value: item.city,
-                            country: item.country,
-                            img: item.img,
-                            url: item.url
-                        };
-                    })
-                });
+// Set the Options for "Bloodhound" Engine
+        var my_Suggestion_class = new Bloodhound({
+            limit: 10,
+            datumTokenizer: Bloodhound.tokenizers.obj.whitespace('value'),
+            queryTokenizer: Bloodhound.tokenizers.whitespace,
+            local: $.map(places, function(item) {
+                //console.log(item.city);
+                return {
+                    value: item.city,
+                    country: item.country,
+                    img: item.img,
+                    url: item.url
+                };
+            })
+        });
 
-        //Initialize the Suggestion Engine
-                my_Suggestion_class.initialize();
+//Initialize the Suggestion Engine
+        my_Suggestion_class.initialize();
 
-                var typeahead_elem = $('.typeahead');
-                typeahead_elem.typeahead({
-                    hint: true,
-                    highlight: true,
-                    minLength: 1
-                },
-                {
-                    name: 'value',
-                    displayKey: 'value',
-                    source: my_Suggestion_class.ttAdapter(),
-                    templates: {
-                        empty: [
-                            '<div class="noitems">',
-                            'No Items Found',
-                            '</div>'
-                        ].join('\n'),
-                        suggestion: function (data) {
-                            return '<a href="' + data.url + '"><img src="' + data.img + '"><span>' + data.value + ', ' + data.country + '</span></a>';
-                        } 
-                           
-                    }
-                });
-            });
+        var typeahead_elem = $('.typeahead');
+        typeahead_elem.typeahead({
+            hint: true,
+            highlight: true,
+            minLength: 1
+        },
+        {
+            name: 'value',
+            displayKey: 'value',
+            source: my_Suggestion_class.ttAdapter(),
+            templates: {
+                empty: [
+                    '<div class="noitems">',
+                    'No Items Found',
+                    '</div>'
+                ].join('\n'),
+                suggestion: function (data) {
+                    return '<a href="' + data.url + '"><img src="' + data.img + '"><span>' + data.value + ', ' + data.country + '</span></a>';
+                }
+            }
+        });
+    });
+
+
+
+window.onload = function() {
+
+// Mobile nav chevron rotation
+var menuElem = document.getElementById('collapseListGroupHeading1');
+
+    menuElem.onclick = function() {
+      this.classList.toggle('open');
+    };
+
+// Mobile search
+var mobSearchTrigger = document.getElementById('search-trigger');
+var mobSearchForm = document.getElementById('header__search-form');
+
+    mobSearchTrigger.addEventListener("click", function() {
+      document.getElementById('header__logo').classList.toggle('hide');
+      document.getElementById('nav-trigger-label').classList.toggle('hide');
+      mobSearchForm.classList.toggle('search-form_mob');
+      this.classList.toggle('active');
+      document.getElementById('main-container').classList.toggle('active');
+    });
+}
